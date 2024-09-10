2df19bffe9ad7a3d8fd507f12822cd3be31817c1
ChinmayiAchanta <achantachinmayi@gmail.com>
2024-09-10 14:44:50 -0400

    v1

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,23 @@
+function doLogin() {
+    const username = document.getElementById('loginName').value;
+    const password = document.getElementById('loginPassword').value;
+    
+    if (username && password) {
+        document.getElementById('loginResult').innerHTML = "Logged In!";
+    } 
+    else {
+        document.getElementById('loginResult').innerHTML = "Please enter a valid username and password.";
+    }
+}
 
+function createAccount() {
+    const username = document.getElementById('createUsername').value;
+    const password = document.getElementById('createPassword').value;
+    
+    if (username && password) {
+        document.getElementById('loginResult').innerHTML = "Account Created!";
+    } 
+    else {
+        document.getElementById('loginResult').innerHTML = "Please enter a valid username and password.";
+    }
+}
