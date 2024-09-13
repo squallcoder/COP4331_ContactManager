76ac67560679e83a36da884433857df69c3a23a4
squallcoder <tammapavan38@gmail.com>
2024-09-13 05:21:29 -0400

    updated the database user for the Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	$conn = new mysqli("localhost", "Admin", "Team7", "SmallProject"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
