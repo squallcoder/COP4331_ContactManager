1f7d482a65af8d0706dc38ab11f5d73c6091317d
MattCarvajal <ma449997@ucf.edu>
2024-09-13 12:15:49 -0400

    add contact first commit

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,28 @@
+<?php
+
+// i need to connect to the database, idk how?
+    $inData = getRequestInfo();
+
+// create contact function
+function addContact($id, $firstName, $lastName, $phoneNumber, $email){
+    $firstName = mysqli_real_escape_string($conn, $firstName);
+    $lastName = mysqli_real_escape_string($conn, $lastName);
+    $phoneNumber = mysqli_real_escape_string($conn, $phoneNumber);
+    $email = mysqli_real_escape_string($conn, $email);
+
+    // insert contact into the database CHECK THIS
+    $query = "INSERT INTO contacts (id, firstName, lastName, phoneNumber, email) VALUES ('$id', '$firstName', '$lastName', '$phoneNumber', '$email')";
+
+    // checking if contact was created successfully by running a query
+    if (mysqli_query($conn, $query)){
+        echo "Contact successfully created!";
+
+    } else{
+        echo "Error creating contact.... please try again" . mysqli_error($conn);
+    }
+}
+
+//finally call the function to add the contact
+addContact($id, $firstName, $lastName, $phoneNumber, $email);
+
+?>
