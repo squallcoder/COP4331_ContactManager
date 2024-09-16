fde9b32a50b96dc07c2ffb5514d0a21c68068b1e
Colin <59761458+masirith@users.noreply.github.com>
2024-09-15 21:43:25 -0400

    Updated AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -8,18 +8,20 @@ if( $conn->connect_error )
 else
 {
     // call the function to add the contact
-    addContact($id, $firstName, $lastName, $phoneNumber, $email);
+    addContact($FirstName, $LastName, $Phone, $Email, $UserID);
 }
 
 // create contact function
-function addContact($id, $firstName, $lastName, $phoneNumber, $email){
-    $firstName = mysqli_real_escape_string($conn, $firstName);
-    $lastName = mysqli_real_escape_string($conn, $lastName);
-    $phoneNumber = mysqli_real_escape_string($conn, $phoneNumber);
-    $email = mysqli_real_escape_string($conn, $email);
+function addContact($FirstName, $LastName, $Phone, $Email, $UserID){
+    $FirstName = mysqli_real_escape_string($conn, $FirstName);
+    $LastName = mysqli_real_escape_string($conn, $LastName);
+    $Phone = mysqli_real_escape_string($conn, $Phone);
+    $Email = mysqli_real_escape_string($conn, $Email);
+    $UserID = mysqli_real_escape_string($conn, $UserID);
 
     // insert contact into the database CHECK THIS
-    $query = "INSERT INTO contacts (id, firstName, lastName, phoneNumber, email) VALUES ('$id', '$firstName', '$lastName', '$phoneNumber', '$email')";
+    /* UPDATED SYNTAX AND VARIABLES -c*/
+    $query = "INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES ('$FirstName', '$LastName', '$Phone', '$Email', '$UserID')";
 
     // checking if contact was created successfully by running a query
     if (mysqli_query($conn, $query)){
