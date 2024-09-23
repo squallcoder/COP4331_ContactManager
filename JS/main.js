6e4ab436838bfd8199b07f19f85109f9ab6c2b40
ZachariaAlaoui <alaoui.zacharia@gmail.com>
2024-09-22 21:00:23 -0400

    Added a piece of code to line 50

--- JS/main.js
+++ JS/main.js
@@ -440,9 +440,6 @@ function updateContact() {
                 //alert(xhr.responseText);
                 console.log(jsonPayload);
                 let jsonObject = JSON.parse(xhr.responseText);
-
-                document.getElementById('updateResult').innerHML = "You have successfully updated your contact!"
-
             }
         };
         xhr.send(jsonPayload);
@@ -450,7 +447,8 @@ function updateContact() {
     catch (err) {
         document.getElementById("updateResult").innerHTML = err.message;
     }
-;
+    document.getElementById('updateResult').innerHML = "You have successfully updated your contact!"
+
 
 }
 
