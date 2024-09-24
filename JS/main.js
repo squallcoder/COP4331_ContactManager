b513f25da57de8e8d349e14cea0b2f310fb529b1
ZachariaAlaoui <alaoui.zacharia@gmail.com>
2024-09-23 22:59:21 -0400

    Testing if display contacts is working

--- JS/main.js
+++ JS/main.js
@@ -226,15 +226,15 @@ function displayContacts() {
                 contactArray = jsonObject.contacts;
                 console.log(contactArray);
 
-                // contactArray.forEach((contact, index) => {
-                //     const contactItem = document.createElement('div');
-                //     contactItem.className = 'contact-item';
-                //     contactItem.innerText = contact.name;
-                //     contactItem.onclick = function () {
-                //         openModal(index);
-                //     };
-                //     contactList.appendChild(contactItem);
-                // });
+                contactArray.forEach((contact, index) => {
+                    const contactItem = document.createElement('div');
+                    contactItem.className = 'contact-item';
+                    contactItem.innerText = contact.FirstName.concat(" ", contact.LastName);
+                    contactItem.onclick = function () {
+                        openModal(index);
+                    };
+                    contactList.appendChild(contactItem);
+                });
             }
         };
         xhr.send(jsonPayload);
