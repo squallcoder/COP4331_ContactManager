50e907bdeb428f7ce7c6f5c1a5fbb2caa814c6d7
ChinmayiAchanta <achantachinmayi@gmail.com>
2024-09-24 23:57:19 -0400

    test new update

--- JS/main.js
+++ JS/main.js
@@ -291,7 +291,7 @@ function showContactToEdit() {
     const firstName = document.getElementById('oldFirstName').value;
 	const lastName = document.getElementById('oldLastName').value;
 
-    const contact = contactArray.find(c => (c.FirstName.toLowerCase() === firstName.toLowerCase()) && (c.LastName.toLowerCase() === lastName.toLowerCase()));
+    const contact = contactArray.find(c => c.FirstName.toLowerCase() === firstName.toLowerCase());
 
     if (contact) {
         closeSelectContactModal();
